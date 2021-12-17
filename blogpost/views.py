00126c09e2a328c174e5ce0d4b7c289c71587a35
pramitsingh0 <pramitsingh0@gmail.com>
2021-12-17 17:24:48 +0530

    Completed homepage

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blogspot/index.html', {})
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'blogpost/index.html', {
+        'posts': posts
+    })
